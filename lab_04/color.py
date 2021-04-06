33b32335a16cfc08f0e08b49082794dcde5783f4
d_ivahnencko <ivahnencko01@gmail.com>
2021-04-06 19:16:42 +0300

    init vers

---
+++ lab_04/color.py
@@ -0,0 +1,31 @@
+from enum import Enum
+from PyQt5.QtGui import QColor
+from PyQt5.QtCore import Qt
+
+
+class Color(Enum):
+    BACK = 0
+    RED = 1
+    BLUE = 2
+    BLACK = 3
+    GREEN = 4
+
+    def __str__(self) -> str:
+        interp = {
+            Color.BACK: 'Фоновый',
+            Color.RED: 'Красный',
+            Color.BLUE: 'Синий',
+            Color.BLACK: 'Черный',
+            Color.GREEN: 'Зеленый'
+        }
+        return interp[self]
+
+    def toQcolor(self) -> QColor:
+        interp = {
+            Color.BACK: Qt.white,
+            Color.RED: Qt.red,
+            Color.BLUE: Qt.blue,
+            Color.BLACK: Qt.black,
+            Color.GREEN: Qt.green
+        }
+        return QColor(interp[self])
