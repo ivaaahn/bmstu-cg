8d72413a6e70af008095fbbced07632940e10238
d_ivahnencko <ivahnencko01@gmail.com>
2021-04-13 20:37:56 +0300

    Refactor Algorithms

--- lab_04/algorithms.py
+++ lab_04/algorithms.py
@@ -72,7 +72,7 @@ class Algorithms:
         return [Algorithms.get_method(Figure.ELLIPSE, way) for way in Way.get_all()[:-1]]
 
     @staticmethod
-    def _extend_with_mirrored_ext(points: List[Point], x: int, y: int, cx: int, cy: int):
+    def Plot8CirclePoints(points: List[Point], x: int, y: int, cx: int, cy: int):
         points.extend([
             Point(cx+x, cy+y),
             Point(cx+x, cy-y),
@@ -86,7 +86,7 @@ class Algorithms:
         ])
 
     @staticmethod
-    def _extend_with_mirrored(points: List[Point], x: int, y: int, cx: int, cy: int):
+    def Plot4EllipsePoints(points: List[Point], x: int, y: int, cx: int, cy: int):
         points.extend([
             Point(cx+x, cy+y),
             Point(cx+x, cy-y),
@@ -103,7 +103,7 @@ class Algorithms:
 
         for x in range(0, utils.round(radius / sqrt(2))+1):
             y = utils.round(sqrt(rsqr-x*x))
-            Algorithms._extend_with_mirrored_ext(
+            Algorithms.Plot8CirclePoints(
                 result, x, y, center.x, center.y)
         return result
 
@@ -117,7 +117,7 @@ class Algorithms:
         for t in arange(0, pi/4+step, step):
             x = utils.round(radius*cos(t))
             y = utils.round(radius*sin(t))
-            Algorithms._extend_with_mirrored_ext(
+            Algorithms.Plot8CirclePoints(
                 result, x, y, center.x, center.y)
         return result
 
@@ -129,37 +129,43 @@ class Algorithms:
 
         # delta - квадрат расстояния до идеальной окружности от диагонального пикселя
         # delta = (x_i + 1)^2 + (y_i - 1)^2 - R^2
-        x, y, delta = 0, radius, 2*(1-radius)
+        x, y, circle_error = 0, radius, 2*(1-radius)
 
-        Algorithms._extend_with_mirrored_ext(result, x, y, center.x, center.y)
+        Algorithms.Plot8CirclePoints(result, x, y, center.x, center.y)
 
         # Только 1/8 окружности
         while x < y:
-            if delta < 0:
-                # delta_tmp - разность расстояний до горизонтального и диагональнго пикселей
-                delta_tmp = 2*(delta+y)-1
-                x += 1
 
-                # Окружность ближе к горизонтальному пикселю
-                if delta_tmp <= 0:      # x, y = x+1, y
-                    delta += 2*x+1
-                else:                   # x, y = x+1, y-1
+            # Диагональный пиксель оказывается внутри реальной окружности
+            if circle_error <= 0:
+                x += 1
+                
+                # delta_tmp - разность расстояний до горизонтального и диагональнго пикселей
+                delta = 2*(circle_error + y) - 1
+
+                # Реальная окружность ближе к диагональному пикселю
+                if delta > 0:
                     y -= 1
-                    delta += 2*(x-y+1)
-            elif delta > 0:
-                delta_tmp = 2*(delta-x)-1
+                    circle_error -= 2 * y - 1
+                
+                circle_error += 2 * x + 1
+
+
+            # Диагональный пиксель лежит вне реальной окружности
+            elif circle_error > 0:
                 y -= 1
 
-                if delta_tmp <= 0:      # x, y = x+1, y-1
-                    x += 1
-                    delta += 2*(x-y+1)
-                else:                   # x, y = x, y-1
-                    delta -= 2*y-1
-            else:
-                x, y = x+1, y-1
-                delta += 2*(x-y+1)
+                # delta_tmp - разность расстояний до диагональнго и вертикального пикселей
+                delta = 2*(circle_error - x) - 1
 
-            Algorithms._extend_with_mirrored_ext(
+                # Реальная окружность ближе к диагональному пикселю
+                if delta <= 0:
+                    x += 1 
+                    circle_error += 2 * x + 1 
+                    
+                circle_error -= 2 * y - 1
+
+            Algorithms.Plot8CirclePoints(
                 result, x, y, center.x, center.y)
 
         return result
@@ -171,7 +177,7 @@ class Algorithms:
         result: List[Point] = []
         x, y, delta = 0, radius, 1-radius
 
-        Algorithms._extend_with_mirrored_ext(result, x, y, center.x, center.y)
+        Algorithms.Plot8CirclePoints(result, x, y, center.x, center.y)
         while x < y:
             x += 1
 
@@ -181,7 +187,7 @@ class Algorithms:
 
             delta += 2 * x + 1  # (2x+3)
 
-            Algorithms._extend_with_mirrored_ext(
+            Algorithms.Plot8CirclePoints(
                 result, x, y, center.x, center.y)
         return result
 
@@ -191,29 +197,40 @@ class Algorithms:
     def canonical_ellipse(center: Point, rx: int, ry: int) -> List[Point]:
         result: List[Point] = []
 
+        f = open('test.txt', 'w')
+
+        rxsqr, rysqr = rx*rx, ry*ry        
+        stopping_x = utils.round(rxsqr/sqrt(rxsqr+rysqr))
+        stopping_y = utils.round(rysqr/sqrt(rxsqr+rysqr))
+
         m: float = ry/rx
-        rxsqr, rysqr = rx*rx, ry*ry
+        x: int = 0
+        while x <= stopping_x:
+            f.write(f'Non-round = {m*sqrt(rxsqr-x*x)}\n')
 
-        limit = utils.round(rxsqr/sqrt(rxsqr+rysqr))
-
-        for x in range(limit+1):
             y = utils.round(m * sqrt(rxsqr-x*x))
-            Algorithms._extend_with_mirrored(result, x, y, center.x, center.y)
+            f.write(f'({x}, {y})\n')
+            Algorithms.Plot4EllipsePoints(result, x, y, center.x, center.y)
+            x += 1
 
+        f.write('CHANGE\n')
 
-        limit = utils.round(rysqr/sqrt(rxsqr+rysqr))
         m: float = rx/ry
-
-        for y in range(limit+1):
+        y: int = 0
+        while y < stopping_y:
             x = utils.round(m * sqrt(rysqr-y*y))
-            Algorithms._extend_with_mirrored(result, x, y, center.x, center.y)
+            f.write(f'({x}, {y})\n')
+            Algorithms.Plot4EllipsePoints(result, x, y, center.x, center.y)
+            y += 1
 
         return result
 
     def param_ellipse(center: Point, rx: int, ry: int) -> List[Point]:
         result: List[Point] = []
 
-        limit_y = utils.round(ry*ry/sqrt(rx*rx+ry*ry))
+        rysqr, rxsqr = ry*ry, rx*rx
+
+        limit_y = utils.round(rysqr/sqrt(rxsqr+rysqr))
 
         x, y, t = rx, 0, 0
         step_x: float = 1/rx
@@ -223,114 +240,142 @@ class Algorithms:
 
         while x > x_end:
             x, y = utils.round(rx*cos(t)), utils.round(ry*sin(t))
-            Algorithms._extend_with_mirrored(result, x, y, center.x, center.y)
+            Algorithms.Plot4EllipsePoints(result, x, y, center.x, center.y)
 
             t += step_y if (y <= limit_y) else step_x
 
         return result
-
-    # def bresenham_ellipse(center: Point, rx: int, ry: int) -> List[Point]:
-    #     if rx == 0 and ry == 0:
-    #         return [center.copy()]
-
-    #     result: List[Point] = []
-    #     rxsqr, rysqr = rx*rx, ry*ry
-
-    #     # Error(x, y) = x^2 * ry^2 + y^2 * rx^2 - rx^2*ry^2
-    #     # delta == Error(x+1, y-1)
-    #     x, y, delta = 0, ry, rxsqr+rysqr-2*rxsqr*ry
-
-    #     Algorithms._extend_with_mirrored(result, x, y, center.x, center.y)
-
-    #     #? OR y >= 0 ?
-    #     while y > 0:
-    #         if delta < 0:
-    #             # delta_tmp - разность расстояний до горизонтального и диагональнго пикселей
-    #             delta_tmp = 2*(delta + y*rxsqr) - rxsqr
-    #             x += 1
-
-    #             # Окружность ближе к горизонтальному пикселю
-    #             if delta_tmp <= 0:      # x, y = x+1, y
-    #                 delta += rysqr*(2*x + 1)
-    #             else:                   # x, y = x+1, y-1
-    #                 y -= 1
-    #                 delta += rysqr*(2*x + 1) + rxsqr*(1 - 2*y)
-
-    #         elif delta > 0:
-    #             delta_tmp = 2*(delta - rysqr*x) - rysqr
-    #             y -= 1
-
-    #             if delta_tmp <= 0:      # x, y = x+1, y-1
-    #                 x += 1
-    #                 delta += rysqr*(2*x + 1) + rxsqr*(1 - 2*y)
-    #             else:                   # x, y = x, y-1
-    #                 delta += rxsqr*(1 - 2*y)
-                    
-    #         else:
-    #             x, y = x+1, y-1
-    #             delta += rysqr*(2*x+1) + rxsqr*(1-2*y)
-
-    #         Algorithms._extend_with_mirrored(
-    #             result, x, y, center.x, center.y)
-
-    #     return result
-
 
     def bresenham_ellipse(center: Point, rx: int, ry: int) -> List[Point]:
         if rx == 0 and ry == 0:
             return [center.copy()]
 
         result: List[Point] = []
-        rx_sqr, ry_sqr = rx*rx, ry*ry
-        two_rx_sqr, two_ry_sqr = 2*rx*rx, 2*ry*ry
+        rxsqr, rysqr = rx*rx, ry*ry
+        two_rxsqr, two_rysqr = 2*rxsqr, 2*rysqr
 
         # Error(x, y) = x^2 * ry^2 + y^2 * rx^2 - rx^2*ry^2
         # delta == Error(x+1, y-1)
+        x, y, ellipse_error = 0, ry, rxsqr+rysqr-two_rxsqr*ry
 
-        x, y = rx, 0
-        x_change, y_change = ry_sqr*(1-2*rx), rx_sqr
-        delta = 0
-        stop_x, stop_y = two_ry_sqr*rx, 0
+        f = open('bres.txt', 'w')
 
-        while(stop_x >= stop_y):
-            Algorithms._extend_with_mirrored(result, x, y, center.x, center.y)
+        Algorithms.Plot4EllipsePoints(result, x, y, center.x, center.y)
+        f.write(f'({x}, {y})\n')
 
-            y += 1
-            stop_y += two_rx_sqr
-            delta += y_change
-            y_change += two_rx_sqr
+        while y > 0:
+            # Диагональный пиксель лежит внутри реального эллипса
+            if ellipse_error <= 0:
+                x += 1
 
-            if 2*delta + x_change > 0:
-                x -= 1
-                stop_x -= two_ry_sqr
-                delta += x_change
-                x_change += two_ry_sqr
+                # Разность расстояний до горизонтального и диагональнго пикселей
+                delta = 2*ellipse_error + rxsqr * (2 * y - 1)
+                f.write(f'delta = {delta}\n')
+                
 
-        x, y = 0, ry
-        x_change, y_change = ry_sqr, rx_sqr*(1-2*ry)
-        delta = 0
-        stop_x, stop_y = 0, two_rx_sqr*ry
-        
-        while(stop_x <= stop_y):
-            Algorithms._extend_with_mirrored(result, x, y, center.x, center.y)
-
-            x += 1
-            stop_x += two_ry_sqr
-            delta += x_change
-            x_change += two_ry_sqr
-
-            if 2*delta + y_change > 0:
+                # Реальный эллипс ближе к диагональному пикселю
+                if delta >= 0:
+                    y -= 1
+                    ellipse_error += rxsqr * (1 - 2 * y)
+                    
+                ellipse_error += rysqr * (2 * x + 1)
+                
+            # Диагональный пиксель лежит снаружи реального эллипса
+            elif ellipse_error > 0:
                 y -= 1
-                stop_y -= two_rx_sqr
-                delta += y_change
-                y_change += two_rx_sqr
+                
+                # Разность расстояний до диагональнго и вертикального пикселей
+                delta = 2*ellipse_error - rysqr * (2 * x + 1)
+                        
+                # Реальный эллипс ближе к диагональному пикселю
+                if delta < 0:
+                    x += 1
+                    ellipse_error += rysqr * (2 * x + 1)
+
+                ellipse_error += rxsqr * (1 - 2 * y)
+
+            f.write(f'({x}, {y})\n')
+            Algorithms.Plot4EllipsePoints(result, x, y, center.x, center.y)
 
         return result
 
 
+    # def bresenham_ellipse(center: Point, rx: int, ry: int) -> List[Point]:
+    #     if rx == 0 and ry == 0:
+    #         return [center.copy()]
 
-    def midpoint_ellipse(center: Point, a: int, b: int) -> List[Point]:
+    #     result: List[Point] = []
+    #     rx_sqr, ry_sqr = rx*rx, ry*ry
+    #     two_rx_sqr, two_ry_sqr = 2*rx*rx, 2*ry*ry
+
+    #     # Error(x, y) = x^2 * ry^2 + y^2 * rx^2 - rx^2*ry^2
+    #     # delta == Error(x+1, y-1)
+
+    #     x, y = rx, 0
+    #     x_change, y_change = ry_sqr*(1-2*rx), rx_sqr
+    #     delta = 0
+    #     stop_x, stop_y = two_ry_sqr*rx, 0
+
+    #     while(stop_x >= stop_y):
+    #         Algorithms.Plot4EllipsePoints(result, x, y, center.x, center.y)
+
+    #         y += 1
+    #         stop_y += two_rx_sqr
+    #         delta += y_change
+    #         y_change += two_rx_sqr
+
+    #         if 2*delta + x_change > 0:
+    #             x -= 1
+    #             stop_x -= two_ry_sqr
+    #             delta += x_change
+    #             x_change += two_ry_sqr
+
+    #     x, y = 0, ry
+    #     x_change, y_change = ry_sqr, rx_sqr*(1-2*ry)
+    #     delta = 0
+    #     stop_x, stop_y = 0, two_rx_sqr*ry
+        
+    #     while(stop_x <= stop_y):
+    #         Algorithms.Plot4EllipsePoints(result, x, y, center.x, center.y)
+
+    #         x += 1
+    #         stop_x += two_ry_sqr
+    #         delta += x_change
+    #         x_change += two_ry_sqr
+
+    #         if 2*delta + y_change > 0:
+    #             y -= 1
+    #             stop_y -= two_rx_sqr
+    #             delta += y_change
+    #             y_change += two_rx_sqr
+
+    #     return result
+
+
+
+    def midpoint_ellipse(center: Point, rx: int, ry: int) -> List[Point]:
         pass
+        # if rx == 0 or ry == 0:
+        #     return [center.copy()]
+
+
+        # result: List[Point] = []
+        # rxsqr, rysqr = rx*rx, ry*ry
+        # x, y, delta = 0, ry, rysqr - rxsqr*
+
+        # Algorithms.Plot8CirclePoints(result, x, y, center.x, center.y)
+        # while x < y:
+        #     x += 1
+
+        #     if delta >= 0:
+        #         y -= 1
+        #         delta -= 2 * y  # (2y-2)
+
+        #     delta += 2 * x + 1  # (2x+3)
+
+        #     Algorithms.Plot8CirclePoints(
+        #         result, x, y, center.x, center.y)
+        # return result
 
     def library_ellipse(center: Point,  a: int, b: int) -> None:
         return None
