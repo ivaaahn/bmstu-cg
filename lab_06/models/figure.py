4fa28664704605735c1ca3e3a650edaa5fdea15b
d_ivahnencko <ivahnencko01@gmail.com>
2021-05-04 14:35:09 +0300

    REMOVE: extra func

--- lab_06/models/figure.py
+++ lab_06/models/figure.py
@@ -1,4 +1,3 @@
-import utils
 from properties.color import Color
 from .point import Point
 from .polygon import Polygon
@@ -8,9 +7,6 @@ class Figure:
     def __init__(self, color: Color = Color.BLACK) -> None:
         self._color = color
         self._data: list[Polygon] = [Polygon()]
-        self.p_min = Point(utils.W + 10, utils.H + 10)
-        self.p_max = Point(-10, -10)
-
         self.seed_pixels: list[Point] = []
 
     def __bool__(self) -> bool:
@@ -23,8 +19,6 @@ class Figure:
         return len(self._data) == 1 and self.last_polygon.size() == 0
 
     def clear(self) -> None:
-        self.p_min = Point(100000, 100000)
-        self.p_max = Point(-100000, -100000)
         self._data = [Polygon()]
         self.seed_pixels.clear()
 
@@ -48,12 +42,6 @@ class Figure:
         return self._data[-1]
 
     def add_vertex(self, vertex: Point):
-        self.p_max.x = max(vertex.x, self.p_max.x)
-        self.p_max.y = max(vertex.y, self.p_max.y)
-
-        self.p_min.x = min(vertex.x, self.p_min.x)
-        self.p_min.y = min(vertex.y, self.p_min.y)
-
         self.last_polygon.add_vertex(vertex)
 
     def close_this_polygon(self) -> None:
